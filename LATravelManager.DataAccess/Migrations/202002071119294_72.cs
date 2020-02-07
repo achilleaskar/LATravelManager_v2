f1b637f6a2603e689fd5cfece4d59da11ff0fbee
Achillleas Karagiannis <achilleaskaragiannis@outlook.com>
2020-02-07 13:23:24 +0200

    gia pes

---
+++ LATravelManager.DataAccess/Migrations/202002071119294_72.cs
@@ -0,0 +1,32 @@
+﻿namespace LATravelManager.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _72 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Bookings", "ProformaSent", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Bookings", "RecieptSent", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Personal_Booking", "VoucherSent", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Personal_Booking", "ProformaSent", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Personal_Booking", "RecieptSent", c => c.Boolean(nullable: false));
+            AddColumn("dbo.ThirdParty_Booking", "VoucherSent", c => c.Boolean(nullable: false));
+            AddColumn("dbo.ThirdParty_Booking", "ProformaSent", c => c.Boolean(nullable: false));
+            AddColumn("dbo.ThirdParty_Booking", "RecieptSent", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ThirdParty_Booking", "RecieptSent");
+            DropColumn("dbo.ThirdParty_Booking", "ProformaSent");
+            DropColumn("dbo.ThirdParty_Booking", "VoucherSent");
+            DropColumn("dbo.Personal_Booking", "RecieptSent");
+            DropColumn("dbo.Personal_Booking", "ProformaSent");
+            DropColumn("dbo.Personal_Booking", "VoucherSent");
+            DropColumn("dbo.Bookings", "RecieptSent");
+            DropColumn("dbo.Bookings", "ProformaSent");
+        }
+    }
+}
